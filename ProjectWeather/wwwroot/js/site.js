dd9b2b38dfbe79c34aae45209820dff42626b244
eness <eness@DESKTOP-PK3SG8H>
2022-01-17 12:06:50 +0300

    Add project files.

---
+++ ProjectWeather/wwwroot/js/site.js
@@ -0,0 +1,29 @@
+﻿////$(document).ready(function () {
+////    alert("jsdfjksdf")
+////    $("#form-submit").submit(function (event) {
+////        performSearch(event);
+////    });
+////});
+
+
+////function performSearch(event) {
+////    var request;
+////    event.preventDefault();
+
+////    request = $.ajax({
+////        url: 'https://www.metaweather.com/api/location/search/?query=san',
+////        type: "GET",
+////        data: {
+////            q: $("#city").val()
+
+////        }
+////    });
+
+////    request.done(function (response) {
+////        formatSearch(response);
+////    });
+////}
+
+////function formatSearch(jsonObject) {
+////    var city_name = jsonObject.latt_long[0];
+////}
